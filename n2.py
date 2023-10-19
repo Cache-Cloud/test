815c26f3218c22afc492845903eeefadda4de57f
乔栀 <36266781+Cache-Cloud@users.noreply.github.com>
2023-10-19 21:37:51 +0800

    233

--- n2.py
+++ n2.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 n1 = np.array([1,2])
@@ -7,5 +6,6 @@ print(n1)
 print(n2)
 print(n1*n2)
 print(n1**n2)
-help(np.arange)
-help(np.reshape)
+pd.DataFrame(np.arange(1,10).reshape(3,3))
+print(np.arange(1,10).reshape(3,3))
+print(np.arange(1,10).reshape(3,3))
